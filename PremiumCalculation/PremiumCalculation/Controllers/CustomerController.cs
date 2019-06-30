460fd0b59abdcca52d24b014e512ea460a8a4dc6
pavanssvr <39695321+pavanssvr@users.noreply.github.com>
2019-06-30 20:35:17 +1000

    Update CustomerController.cs

--- PremiumCalculation/PremiumCalculation/Controllers/CustomerController.cs
+++ PremiumCalculation/PremiumCalculation/Controllers/CustomerController.cs
@@ -17,24 +17,48 @@ namespace PremiumCalculation.Controllers
         
         public ActionResult Enter()
         {
-            ViewBag.Occupations = GetListItems();
+            try
+            {
+                ViewBag.Occupations = GetListItems();
+            }
+            catch(Exception ex)
+            {
+               ///TODO : Handle the exception and show the proper message
+            } 
             return View("CalculatePremium");
+
         }
 
         //This action mehtod will be called on button submission        
         public ActionResult CalculatePremium(Customer objCustomer)
         {
-            ViewBag.Occupations = GetListItems();
-            ViewData["PremiumAmount"] = Math.Round(_repository.CalculateDeathPremium(objCustomer), 2);
+            try
+            {
+                ViewBag.Occupations = GetListItems();
+                ViewData["PremiumAmount"] = Math.Round(_repository.CalculateDeathPremium(objCustomer), 2);
+            }
+            catch (Exception ex)
+            {
+                ///TODO : Handle the exception and show the proper message
+            }
             return View();
         }
 
         //This action mehtod will be called on Occupation dropdown change 
         public JsonResult CalculatePremiumJson(Customer objCustomer)
         {
-            ViewData["PremiumAmount"] = "";
-            ViewBag.Occupations = GetListItems();
-            return Json(new { PremiumAmount = Math.Round(_repository.CalculateDeathPremium(objCustomer), 2) }, JsonRequestBehavior.AllowGet);
+            decimal premiumAmount = 0;
+            try
+            {
+                ViewData["PremiumAmount"] = "";
+                ViewBag.Occupations = GetListItems();
+                premiumAmount = Math.Round(_repository.CalculateDeathPremium(objCustomer), 2);
+            }
+            catch (Exception ex)
+            {
+                ///TODO : Handle the exception and show the proper message
+            }
+            return Json(new { PremiumAmount = premiumAmount }, JsonRequestBehavior.AllowGet);
         }
 
         private List<SelectListItem> GetListItems()
